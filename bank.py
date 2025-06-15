34b4dab5388ad3e05506af9eb0df663570eb799a
EF-Code <111162892+EF-Code@users.noreply.github.com>
2025-06-15 20:17:38 +0100

    create BankAccount class

---
+++ bank.py
@@ -0,0 +1,5 @@
+class BankAccount:
+    def __init__(self, account_number, holder_name, balance=0):
+        self.account_number = account_number
+        self.holder_name = holder_name
+        self.balance = balance
