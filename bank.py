1ba9e7b43eb0918699d8b9d8116f4ab9cb06ae80
Apochi <emmanuelapochi60@gmail.com>
2025-06-15 20:45:41 +0100

    create savings and current account  class

--- bank.py
+++ bank.py
@@ -14,3 +14,17 @@ class BankAccount:
             self.balance -= amount
             return f"Withdrew {amount}. New balance: {self.balance}"
         return "Insufficient balance or invalid amount."
+
+class SavingsAccount(BankAccount):
+    def __init__(self, account_number, holder_name, balance=0, withdrawal_limit=5000):
+        super().__init__(account_number, holder_name, balance)
+        self.withdrawal_limit = withdrawal_limit
+
+    def withdraw(self, amount):
+        if amount > self.withdrawal_limit:
+            return f"Cannot withdraw more than {self.withdrawal_limit} at a time."
+        return super().withdraw(amount)
+
+
+class CurrentAccount(BankAccount):
+    pass
