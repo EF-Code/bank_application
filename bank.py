2010fa1834585ae019283506447f675c63f7b4f3
Gidado <gidadogodswill40@gmail.com>
2025-06-15 20:22:53 +0100

    added deposit and withdraw method to BankAccount class

--- bank.py
+++ bank.py
@@ -3,3 +3,14 @@ class BankAccount:
         self.account_number = account_number
         self.holder_name = holder_name
         self.balance = balance
+   def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return f"Deposited {amount}. New balance: {self.balance}"
+        return "Invalid deposit amount."
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            return f"Withdrew {amount}. New balance: {self.balance}"
+        return "Insufficient balance or invalid amount."
